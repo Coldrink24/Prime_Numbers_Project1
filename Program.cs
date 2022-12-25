3983ab9c9e075afb868ba1540c79acc27e9d1b3f
Coldrink24 <117348473+Coldrink24@users.noreply.github.com>
2022-12-25 15:17:01 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Program
+{
+    class console
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number.");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number.");
+            int num2 = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("The list of prime numbers between" + num1 + " and " + num2 + " is :");
+
+            for (int i = num1; i <= num2; i++)
+            {
+                if (Check(i) == true)
+                    Console.WriteLine(i);
+
+
+            }
+
+
+        }
+
+        public static bool Check(int i)
+        {
+            bool prime = true;
+
+            if (i == 0 || i == 1)
+                prime = false;
+
+            for (int k = 2; k <= i / 2; k++)
+            {
+
+
+                if ((i % k) == 0) prime = false;
+            }
+            return prime;
+        }
+    }
+
+}
